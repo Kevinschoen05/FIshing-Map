eb89461ba60668104c608756863bc8439416349a
Kevin Schoen <kevinschoen05@gmail.com>
2021-04-30 00:47:25 -0400

    external styles, post request, initial form layout

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded());
 
 app.get("/", (req, res)=> {
   res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", (req, res) => {
+  var species = req.body.fishType;
+  var weight = req.body.weight;
+  var angler = req.body.angler;
+
+  console.log(species + " " + weight + " " + angler)
 })
 
 app.listen(3000, ()=>{
